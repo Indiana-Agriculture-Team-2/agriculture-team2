10ecfeef6947c6577e0d28f8cc99497ebca00719
colejmetzger41 <colejmetzger@gmail.com>
2021-07-16 14:08:19 -0400

    sponsor page update

--- flask-fire/server/src/app.py
+++ flask-fire/server/src/app.py
@@ -8,11 +8,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+class Farmer:
+    def __init__(self, farmName, location, products, delivery):
+        self.farmName = farmName
+        self.location = location
+        self.products = products
+        self.delivery = delivery
+
+## Example Farmer Data
+farmers = ["David Lamb", "Michael Smith", "John Doe", "Sarah Smith", "Joe Brown", "Emma Johnson"]
+farmNames = ["Boulder Valley Lands", "Bull's Eye Farms", "Black Raven Farm", "Meadowland Pastures", "Robinwood Estate", "Stallion Estate"]
+locations = ["Lebonon, IN", "Carmel, IN", "Fort Wayne, IN", "Bluffton, IN", "Bloominton, IN", "Greenfield, IN"]
+products = [["Tomatoes"], ["Corn", "Soybeans"], ["Corn"], ["Squash", "Tomatoes"], ["Carrots", "Squash", "Tomatoes"], ["Soybeans"]]
+delivery = [True, True, False, True, False, False]
 
 @app.route('/sponsor')
 def sponsor():
-    farmers = ["David Lamb", "Michael Smith", "John Doe", "Sarah Smith", "Cole Metzger"]
-    return render_template("sponsor.html", farmers=farmers)
+    return render_template("sponsor.html", farmers=farmers, farmNames=farmNames, locations=locations, products=products, delivery=delivery)
 
 if __name__ == '__main__':
     ##debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080))
